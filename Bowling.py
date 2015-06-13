9a99d319a6e1c96a63a90ec85fe4b90c643c33d4
zeya <zeyaxp@hotmail.com>
2015-06-13 22:31:35 +0800

    extract condition statment to small method

--- Bowling.py
+++ Bowling.py
@@ -11,20 +11,30 @@ class Game:
     def isSpare(self):
         return self.throwBalls[self.ball] + self.throwBalls[self.ball+1] == 10
 
+    def scoreStrike(self):
+        self.score += 10 + self.throwBalls[self.ball+1] +self.throwBalls[self.ball+2];
+        self.ball += 1
+        return True
+
+    def scoreSpare(self):
+        self.score += 10 + self.throwBalls[self.ball+2]
+        self.ball += 2
+
+    def scoreNormal(self):
+        self.score += self.throwBalls[self.ball] + self.throwBalls[self.ball+1]
+        self.ball += 2
+
     def getScore(self, turnNumber):
         turn = 1 
         self.score = 0
         self.ball = 0
         while (turn <= turnNumber):
             if self.isStrike():
-                self.score += 10 + self.throwBalls[self.ball+1] +self.throwBalls[self.ball+2];
-                self.ball += 1                
+                self.scoreStrike(); 
             elif self.isSpare():
-                self.score += 10 + self.throwBalls[self.ball+2]
-                self.ball += 2
+                self.scoreSpare()        
             else:
-                self.score += self.throwBalls[self.ball] + self.throwBalls[self.ball+1]
-                self.ball += 2
+                self.scoreNormal()       
             
             turn += 1
         return self.score
